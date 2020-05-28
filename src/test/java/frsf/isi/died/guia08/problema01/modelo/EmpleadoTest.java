d529404ee98c50caf9b3fc636b62e149b42d7b47
cote <cote.jll@gmail.com>
2020-05-28 18:59:07 -0300

    Guia08 sin test

--- src/test/java/frsf/isi/died/guia08/problema01/modelo/EmpleadoTest.java
+++ src/test/java/frsf/isi/died/guia08/problema01/modelo/EmpleadoTest.java
@@ -2,19 +2,36 @@ package frsf.isi.died.guia08.problema01.modelo;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
+import frsf.isi.died.guia08.problema01.AppRRHH;
+import frsf.isi.died.guia08.problema01.modelo.Empleado.Tipo;
+import frsf.isi.died.guia08.problema01.modelo.exception.NoSePuedeAsignarTareaException;
+
 public class EmpleadoTest {
 
 	// IMPORTANTE
 	// ESTA CLASE ESTA ANOTADA COMO @IGNORE por lo que no ejecutará ningun test
 	// hasta que no borre esa anotación.
 	
+	AppRRHH app;
+
+	@BeforeClass
+	public void init() {
+		app.cargarEmpleadosContratadosCSV("empleadosContratados.csv");
+		app.cargarEmpleadosEfectivosCSV("empleadosEfectivos");
+		app.cargarTareasCSV("tareas.csv");
+	}
+	
 	@Test
-	public void testSalario() {
-		fail("Not yet implemented");
+	public void testSalario() throws NoSePuedeAsignarTareaException {		
+		
+		//123464 = Ariel ; sueldo por hora = 
+		
+		Empleado empleado = app.getEmpleados().stream().filter(e -> e.getCuil() == 123464).findFirst().get();
+		assertTrue(0.0 == empleado.salario());
 	}
 
 	@Test
